81e5f3af3c351b8f2b46faa3fe6c542a5ae26b70
saga3121 <sgaarkadam1312@gmail.com>
2022-08-02 13:28:34 +0530

    correct the code for sum function and added multiplication fumction

---
+++ dataprocessing.py
@@ -0,0 +1,16 @@
+print("adding all data processing code related to fraud detection")
+
+a=5
+b=6
+
+
+def sum(a,b):
+    c=a+b
+    return c
+
+print("sum function")
+
+def multiplication(a,b):
+    d=a*b
+    return d
+print("multiplication function")
